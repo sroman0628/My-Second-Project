e08de177c39a4cbecc14d032cc666bb20982081c
sroman0628 <silvia.roman@chromatechacademy.net>
2025-02-06 21:03:13 -0500

    First push

---
+++ src/main/java/org/example/ExampleThree.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class ExampleThree {
+    public static void main(String[] args) {
+        System.out.println("This is a new class");
+    }
+}
---
+++ src/main/java/org/example/Sample.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Sample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("making changes to the project");
+        System.out.println("Adding new line");
+
+    }
+
+}
